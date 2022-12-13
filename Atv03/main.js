1fe71737da5dddfdcf5dc51675f523d500cf1f0f
Dyeizon <34202471+Dyeizon@users.noreply.github.com>
2022-12-13 08:12:25 -0300

    Add files via upload

--- Atv03/main.js
+++ Atv03/main.js
@@ -2,9 +2,13 @@
 const form = document.querySelector('#form-to-do-list')
 const input = document.querySelector('#input')
 const ulList = document.querySelector('ul#list')
+const darkButton = document.querySelector('#dark')
+const body = document.querySelector('body')
+const h2 = document.querySelector('h2')
 
 // Event Listeners
 form.addEventListener('submit', formSubmit)
+darkButton.addEventListener('click', toggleDark)
 
 // Funções
 function formSubmit(event) {
@@ -26,3 +30,8 @@ function markAsDone() {
     this.classList.toggle('done')
 }
 
+function toggleDark() {
+    body.classList.toggle('dark-mode')
+    h2.classList.toggle('dark-mode')
+
+}
